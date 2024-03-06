4905e46a8a78ad163db0137c5553a9949c8b0cb0
DerrickJ7 <143416844+DerrickJ7@users.noreply.github.com>
2024-03-06 11:18:13 -0500

    draft of models

---
+++ RestRecommendations.java
@@ -0,0 +1,34 @@
+package model.Jobs;
+
+import Util.Utility;
+import com.google.cloud.Timestamp;
+import com.google.cloud.firestore.DocumentReference;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestRecommendations {
+    @Getter
+private List<DocumentReference> assignedUsers;
+    private DocumentReference createdBy;
+
+    public RestRecommendations(List<DocumentReference> assignedUsers, DocumentReference createdBy) {
+        this.assignedUsers = assignedUsers;
+        this.createdBy = createdBy;
+    }
+// Setters and Getters for String parameters to perform Firestore queries
+
+    public void setCreatedBy(String createdBy) {
+        // Perform Firebase Firestore query to retrieve DocumentReference for createBy
+        this.createdBy = Utility.retrieveDocumentReference("Users", createdBy);
+    }
+
+    public void setAssignedUsers(ArrayList<String> assignedUsers) {
+
+        this.assignedUsers = new ArrayList<>();
+        for(String user : assignedUsers) {
+            this.assignedUsers.add(Utility.retrieveDocumentReference("Users", user));
+        }
+    }
+}
