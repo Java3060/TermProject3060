7b0fb1d12e512da68830b2fed924682d70304d73
EssenceJO <122559639+EssenceJO@users.noreply.github.com>
2024-03-21 19:13:49 -0400

    MovedFiles

---
+++ src/main/java/models/Recommendations.java
@@ -0,0 +1,17 @@
+package models;
+
+import com.google.cloud.Timestamp;
+import model.Users;
+
+import java.util.List;
+
+public class Recommendations extends ARecommendations {
+    private List<Users> assignedUsers;
+    private Users createdBy;
+
+    public Recommendations(public ARecommendations( String recId, String title, String description, int movieID, Timestamp recDate) {
+        super(recId,  title,  description, movieID, recDate);
+        this.assignedUsers = assignedUsers;
+        this.createdBy = createdBy;
+    }
+}
