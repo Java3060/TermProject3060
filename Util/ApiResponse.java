f76bd2ac74dba2ff5f010ba6756db41c4c1c72e3
DerrickJ7 <143416844+DerrickJ7@users.noreply.github.com>
2024-03-21 14:40:14 -0400

    Update ApiResponse.java

--- Util/ApiResponse.java
+++ Util/ApiResponse.java
@@ -1,5 +1,5 @@
 package Util;
 
-public record ApiResponse(boolean success, String message, Object data, Object error) {}
+public record ApiResponse<T>(boolean success, String message, Object data, Object error) {}
 
 
